39803247ed822c9687b0b7849e028a947f4830ec
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-21 00:00:46 +0300

    Update feedback form js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formEl = document.querySelector('.feedback-form');
+const inputEl = formEl.querySelector('input');
+const textareaEl = formEl.querySelector('textarea');
+let inputData = {};
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+saveData();
+
+function onFormInput(evt) {
+  inputData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function saveData() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    inputEl.value = savedData.email;
+    textareaEl.value = savedData.message;
+  }
+}
