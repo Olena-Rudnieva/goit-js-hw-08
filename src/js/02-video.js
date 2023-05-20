8935e2ac5035e9136034aa7c9277a04b64a4f868
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-20 20:01:35 +0300

    Add lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,19 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player', {
   width: 640,
 });
 
-player.on('timeupdate', function (evt) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
-});
+player.on(
+  'timeupdate',
+  throttle(function (evt) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(evt.seconds)
+    );
+  }, 1000)
+);
 
 player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
@@ -14,11 +21,9 @@ player
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
