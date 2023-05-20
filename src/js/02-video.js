7a9e285e7148e2a006e983cc741e5a018b74ce39
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-20 19:09:44 +0300

    Update vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  width: 640,
+});
+
+player.on('timeupdate', function (evt) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
+});
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
