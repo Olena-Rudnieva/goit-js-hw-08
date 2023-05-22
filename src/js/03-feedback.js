e934ec1bc9aa9b1148df0a1dbf406ef983857fa8
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-22 15:13:26 +0300

    Update feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,12 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
 const formEl = document.querySelector('.feedback-form');
 const inputEl = formEl.querySelector('input');
 const textareaEl = formEl.querySelector('textarea');
-let inputData = {};
+
+let inputData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 formEl.addEventListener('input', throttle(onFormInput, 500));
 formEl.addEventListener('submit', onFormSubmit);
@@ -19,16 +20,20 @@ function onFormInput(evt) {
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  if (inputEl.value && textareaEl.value) {
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    inputData = {};
+  } else {
+    alert('Заповніть порожнє поле!');
+  }
 }
 
 function saveData() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedData) {
-    inputEl.value = savedData.email;
-    textareaEl.value = savedData.message;
+  if (inputData) {
+    let { email, message } = formEl.elements;
+    email.value = inputData.email || '';
+    message.value = inputData.message || '';
   }
 }
